c3ba1e79a7b8f8737e64a711f2259672a27375e7
SerheiYakimov <ysergey2901@gmail.com>
2021-08-07 17:02:33 +0300

    создал шаблон меню

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import menuItemTemplate from './templates/menu-item.hbs';
+import {menuItem} from './menu.json';
 import './sass/main.scss';
+
+console.log(menuItemTemplate);
+console.log(menuItem);
+const menu = document.querySelector('.js-menu');
+
+const markup = menuItemTemplate(menuItem);
+menu.insertAdjacentHTML('beforeend', markup);
