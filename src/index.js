846f27bb331f93ce755bf7537aa3829d317e952a
SerheiYakimov <ysergey2901@gmail.com>
2021-08-08 09:38:44 +0300

    установил parcel and gh-pages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import menuItemTemplate from './templates/menu-item.hbs';
-import {menuItem} from './menu.json';
-import './sass/main.scss';
+import menuItem from './menu.json';
+import './styles.css';
 
-console.log(menuItemTemplate);
+// console.log(menuItemTemplate);
 console.log(menuItem);
-const menu = document.querySelector('.js-menu');
+const menu = document.querySelector('js-menu');
 
-const markup = menuItemTemplate(menuItem);
-menu.insertAdjacentHTML('beforeend', markup);
+
+
+// const markup = menuItemTemplate(menuItem);
+// menu.insertAdjacentHTML('beforeend', markup);
