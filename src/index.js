74610ab6ffa3863e65f376d33be8a212e2457ba4
SerheiYakimov <ysergey2901@gmail.com>
2021-08-09 00:39:04 +0300

    внес правки в адрес

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-import './sass/main.scss';
+import menuTemplate from './templates/menu-tps.hbs';
+import menuList from './menu.json';
+import './styles.css';
+
+// console.log(menuItemTemplate);
+console.log(menuList);
+const menu = document.querySelector('.js-menu');
+
+function createMarkup(menuList) {
+    return menuList.map(menuTemplate).join('');
+}
+
+console.log(createMarkup);
+
+
+const markup = createMarkup(menuList);
+menu.insertAdjacentHTML('beforeend', markup);
