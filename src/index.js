9fbaf08e1103988e882881939d86213249554088
SerheiYakimov <ysergey2901@gmail.com>
2021-08-09 02:02:12 +0300

    добавил скрипт в index.html

--- src/index.js
+++ src/index.js
@@ -2,16 +2,11 @@ import menuTemplate from './templates/menu-tps.hbs';
 import menuList from './menu.json';
 import './styles.css';
 
-// console.log(menuItemTemplate);
+console.log(menuTemplate);
 console.log(menuList);
 const menu = document.querySelector('.js-menu');
 
-function createMarkup(menuList) {
-    return menuList.map(menuTemplate).join('');
-}
 
-console.log(createMarkup);
-
-
-const markup = createMarkup(menuList);
+const markup = menuList.map(menuTemplate).join('');
+// console.log(markup);
 menu.insertAdjacentHTML('beforeend', markup);
