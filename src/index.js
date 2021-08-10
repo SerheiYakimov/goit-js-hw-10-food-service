81704c654698ecbe34a75742c55b74d44724f3cc
SerheiYakimov <ysergey2901@gmail.com>
2021-08-10 08:01:32 +0300

    добавил проверку localeStorage

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import menuTemplate from './templates/menu-tps.hbs';
 import menuList from './menu.json';
 import './styles.css';
 
-console.log(menuTemplate);
-console.log(menuList);
+// console.log(menuTemplate);
+// console.log(menuList);
 
 const menu = document.querySelector('.js-menu');
 const themeSwitch = document.querySelector('#theme-switch-toggle');
@@ -31,8 +31,23 @@ const Theme = {
           
       }
 
-      
-
-
   }
-  
+
+  // const dataforStorage = JSON.stringify(Theme);
+  // localStorage.setItem("Theme", dataforStorage);
+  // const data = localStorage.getItem("Theme");
+  // console.log('data', JSON.parse(data));
+//  Проверка на первое вхождение на сайт, есть ли данные в localeStorage
+  const storageTheme = (JSON.parse(localStorage.getItem("Theme")));
+  // const currentTheme = storageTheme ? localStorage.setItem(JSON.stringify(Theme)) : storageTheme;
+  console.log('storageTheme', storageTheme);
+  // console.log('currentTheme', currentTheme);
+
+  if (storageTheme === null) {
+    localStorage.setItem("Theme", JSON.stringify(Theme.LIGHT));
+    body.classList.toggle('light-theme');
+  };
+  console.log('storageTheme', storageTheme);
+
+ 
+
