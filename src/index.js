8b0756f594d6af32fb5ea8467ae4d6c19cad209c
SerheiYakimov <ysergey2901@gmail.com>
2021-08-10 06:00:21 +0300

    внес правки в шаблон

--- src/index.js
+++ src/index.js
@@ -4,10 +4,35 @@ import './styles.css';
 
 console.log(menuTemplate);
 console.log(menuList);
+
 const menu = document.querySelector('.js-menu');
+const themeSwitch = document.querySelector('#theme-switch-toggle');
+const body = document.querySelector('body');
 
 
 const markup = menuList.map(menuTemplate).join('');
 
 // console.log(markup);
 menu.insertAdjacentHTML('beforeend', markup);
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
+  themeSwitch.addEventListener('change', onThemeChange);
+
+  function onThemeChange(e) {
+
+    console.log(e.target);
+    e.preventDefault();
+      if (e.target.checked === true) {
+        body.classList.toggle('.dark-theme');
+          
+      }
+
+      
+
+
+  }
+  
